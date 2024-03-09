ec57e6ef700ec77d50e30466c3d3e5ca5dd98ba7
Vital Raicheu <rvitaly1978@gmail.com>
2024-03-09 15:36:13 +0300

    feat: add types, find-one-params

---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export enum ErrorMessage {
+  UserNotExist = 'User does not exist.',
+  UserAlreadyExist = 'A user with the requested login already exists.',
+  PasswordNotCorrect = 'Password is not correct.',
+}
