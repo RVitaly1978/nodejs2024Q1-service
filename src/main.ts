76a96f4bec9875e3abbf36abe89c47b3ffab4de2
Vital Raicheu <rvitaly1978@gmail.com>
2024-03-09 18:11:56 +0300

    feat: update appModule and main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+
+import { AppModule } from './app.module'
+import { PORT } from './helpers'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(PORT)
 }
-bootstrap();
+
+bootstrap()
