d3f0c2328bfd3d8f25c29a87df4e10673f6ede05
Vital Raicheu <rvitaly1978@gmail.com>
2024-03-10 15:49:45 +0300

    feat: add swagger API to track controller

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Put, Delete, Body, Param, ParseUUIDPipe, NotFoundException, HttpCode, HttpStatus } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiTags, ApiOperation, ApiParam, ApiBadRequestResponse, ApiNotFoundResponse } from '@nestjs/swagger'
 
 import { TrackService } from './track.service'
 import { CreateTrackDto } from './dto/create-track.dto'
@@ -12,16 +12,23 @@ import { ErrorMessage } from '../types'
 export class TrackController {
   constructor(private trackService: TrackService) {}
 
+  @ApiOperation({ summary: 'Get tracks list', description: 'Gets all library tracks list' })
   @Get()
   async getAllTracks() {
     return (await this.trackService.getAllTracks())
   }
 
+  @ApiOperation({ summary: 'Add new track', description: 'Add new track information' })
+  @ApiBadRequestResponse({ description: ErrorMessage.BadRequestBodyDescription })
   @Post()
   async create(@Body() dto: CreateTrackDto) {
     return (await this.trackService.create(dto))
   }
 
+  @ApiOperation({ summary: 'Get single track by id', description: 'Get single track by id' })
+  @ApiParam({ name: 'id', format: 'uuid' })
+  @ApiBadRequestResponse({ description: ErrorMessage.BadRequestParamDescription })
+  @ApiNotFoundResponse({ description: ErrorMessage.TrackNotExist })
   @Get(':id')
   async getTrackById(@Param('id', ParseUUIDPipe) id: string) {
     const entry = await this.trackService.getTrackById(id)
@@ -31,6 +38,10 @@ export class TrackController {
     return entry
   }
 
+  @ApiOperation({ summary: 'Update track information', description: 'Update library track information by id' })
+  @ApiParam({ name: 'id', format: 'uuid' })
+  @ApiBadRequestResponse({ description: ErrorMessage.BadRequestParamDescription })
+  @ApiNotFoundResponse({ description: ErrorMessage.TrackNotExist })
   @Put(':id')
   async update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -39,6 +50,10 @@ export class TrackController {
     return (await this.trackService.update(id, dto))
   }
 
+  @ApiOperation({ summary: 'Delete track', description: 'Delete track from library' })
+  @ApiParam({ name: 'id', format: 'uuid' })
+  @ApiBadRequestResponse({ description: ErrorMessage.BadRequestParamDescription })
+  @ApiNotFoundResponse({ description: ErrorMessage.TrackNotExist })
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id', ParseUUIDPipe) id: string) {
