e5ff923c5e4a868ddbc1712f87072315353357b2
Vital Raicheu <rvitaly1978@gmail.com>
2024-03-30 22:14:35 +0300

    feat: update types, add dto

--- src/types.ts
+++ src/types.ts
@@ -14,6 +14,12 @@ export enum ErrorMessage {
 
   BadRequestParamDescription = 'Bad request. Param **id** is invalid (not uuid)',
   BadRequestBodyDescription = 'Bad request. **body** does not contain required fields',
+
+  UnauthorizedDescription = 'Unauthorized. Credentials is not valid.',
+  ForbiddenDescription = 'Forbidden. Credentials is not valid.',
+
+  UnauthorizedRefreshTokenDescription = 'Unauthorized. There is no refreshToken in body.',
+  ForbiddenRefreshTokenDescription = 'Forbidden. Refresh token is invalid or expired.',
 }
 
 export enum SuccessMessage {
@@ -29,4 +35,9 @@ export type UserDb = {
   version: number
   createdAt: Date
   updatedAt: Date
+}
+
+export type JWTTokenPayload = {
+  id: string
+  login: string
 }
